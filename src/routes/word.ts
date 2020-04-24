cdbac7d764d10bd59b2945c42328ff02c4be8bcf
jhony <jhonyvega24@gmail.com>
2020-04-24 09:21:32 -0500

    add logical to route word

--- src/routes/word.ts
+++ src/routes/word.ts
@@ -1,24 +1,33 @@
 import { Router, Request, Response } from "express";
+import firebase from "../config/authFirebase";
+import Word from "../model/Word";
+import { IStatus } from "../types/IStatus";
 
 const router = Router();
+const database: firebase.database.Database = firebase.database();
+const word: Word = new Word(database);
 
 //get all words
 router.get("/", ( req : Request , res : Response) => {
-
+    return word.readAll((data: [Object] & Object) => res.json(data));
 })
 
 //get a wrod according to id
 router.get("/:id", ( req : Request , res: Response) => {
-
+    const { id } = req.params;
+    return word.byId(id, (data: Object) => res.json(data));
 })
 
 // create new word
 router.post("/", ( req : Request , res : Response) => {
-
+    const parameters = req.body;
+    return word.create(parameters, (status: IStatus) => res.json(status));
 })
 
 //increment the points of word
 router.post("/increment", ( req : Request , res : Response) => {
+    const { id } = req.body;
+    return word.incrementPoints(id, (status: IStatus) => res.json(status));
 
 })
 
