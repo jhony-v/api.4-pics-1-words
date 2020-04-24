bbdf66c964bdecd5d0e82da9f8f70d5937bf6a43
jhony <jhonyvega24@gmail.com>
2020-04-24 10:30:00 -0500

    declare api/v1

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import express , { Express } from "express"
 import cors from "cors"
 import compression from "compression"
 import routes from "./routes"
+
 const app : Express = express();
+
 app.use(cors());
 app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-app.use("/api",routes);
+app.use("/api/v1",routes);
 
 
 app.listen( process.env.PORT || 3000 , () => {
